66515009e5fda63fd08fe07129a7ff1aae82ce16
Devansh Mishra <mishradevansh2233@gmail.com>
2024-06-22 16:13:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ let weather = {
         "Wind speed: " + speed + " km/h";
       document.querySelector(".weather").classList.remove("loading");
       document.body.style.backgroundImage =
-        "url('https://unsplash.com/1600x900/?" + name + "')";
+        "url('https://unsplash.com/s/photos/" + name + "')";
     },
     search: function () {
       this.fetchWeather(document.querySelector(".search-bar").value);
